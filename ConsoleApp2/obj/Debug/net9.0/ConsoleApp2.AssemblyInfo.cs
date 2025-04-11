539b3974d3916d06f098cebdcecd97366181917d
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:25:19 +0300

    Update assembly informational version to reflect latest changes

--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6481fae924193e0f89f514b17fe5e24e4cbe5c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9747cf163237aa8ebcdd240231b1c39c61502aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
