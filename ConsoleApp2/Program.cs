2621d355e5a065a2797d16954a4e5234ccb41218
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:31:55 +0300

    Add program to reverse each word in user input

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write any text: ");
+        string text = Console.ReadLine();
+        
+        string reversedWords = ReverseEachWord(text);
+        Console.WriteLine("Reversed text: " + reversedWords);
+    }
+
+    static string ReverseEachWord(string input)
+    {
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] chars = words[i].ToCharArray();
+            Array.Reverse(chars);
+            words[i] = new string(chars);
+        }
+        return string.Join(" ", words);
+    }
+}
