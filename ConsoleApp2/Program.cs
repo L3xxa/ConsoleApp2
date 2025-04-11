f494df703fef15c59c97e83b630d47cda7bcb2f3
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:55:18 +0300

    Add frequency analysis program

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a string for frequency analysis:");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (frequency.ContainsKey(c))
+            {
+                frequency[c]++;
+            }
+            else
+            {
+                frequency[c] = 1;
+            }
+        }
+
+        foreach (var item in frequency)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
