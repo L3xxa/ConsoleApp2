131c2d69012e180cb9428fa4bda95e64e9d1901e
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:44:58 +0300

    Add initial implementation of word count program

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write any text: ");
+        string text = Console.ReadLine();
+        
+        string [] words = text.Split(' ');
+        
+        Console.Write("Enter the word you want to find: ");
+        string findWord = Console.ReadLine();
+        
+        int count = 0;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (findWord == words[i])
+            {
+                count++;
+            }
+        }
+        
+        Console.WriteLine($"The word '{findWord}' appears {count} times in the text.");
+    }
+}
