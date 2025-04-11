cff7a2dafc01aa2472e02b0eb1cbbc6840ac2335
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:38:05 +0300

    Add vowel counting functionality to the console application

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write any text: ");
+        string input = Console.ReadLine();
+
+        int Vowel = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == 'a' || 
+                input[i] == 'e' ||
+                input[i] == 'i' ||
+                input[i] == 'o' ||
+                input[i] == 'u')
+            {
+                Vowel++;
+            }
+            
+            
+        }
+        Console.Write("The number of vowels in the text is: ");
+        Console.WriteLine(Vowel);
+    }
+}
