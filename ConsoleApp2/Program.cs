9747cf163237aa8ebcdd240231b1c39c61502aba
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-11 11:19:43 +0300

    Add basic word count functionality to the console application

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write any text: ");
+        string input = Console.ReadLine();
+        
+        string[] words = input.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        Console.WriteLine("Words in the input:" + words.Length);
+    }
+    
+}
