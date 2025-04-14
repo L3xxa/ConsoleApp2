2a944cfa519e526c49245ac629a3d2bb62edf38b
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-14 20:35:46 +0300

    Add basic text formatting functionality to the console application

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any text:");
+        string text = Console.ReadLine();
+
+        bool behindChar = true;
+        string newText = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                if (behindChar)
+                {
+                    newText += char.ToUpper(text[i]);
+                    behindChar = false;
+                }
+                else
+                {
+                    newText += char.ToLower(text[i]);
+                }
+            }
+            else if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\n')
+            {
+                newText += text[i];
+                behindChar = true;
+            }
+            else
+            {
+                newText += text[i];
+            }
+        }
+        Console.WriteLine($"New text: {newText}");
+    }
+}
