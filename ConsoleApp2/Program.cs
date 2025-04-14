680f6f56b6a99aea7e3978fad363068d3dbf1138
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-14 20:54:15 +0300

    Add text filtering feature to censor indecent words

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any text: ");
+        string text = Console.ReadLine();
+        string[] words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var indecentSet = new HashSet<string>(new[] { "bad", "ugly", "stupid", "idiot", "fool", "dumb", "hate", "moron", "jerk", "bastard", "asshole", "bitch", "damn", "shit", "fuck", "dick", "hell" }, StringComparer.OrdinalIgnoreCase);
+
+        string newText = "";
+        foreach (string word in words)
+        {
+            string cleanWord = Regex.Replace(word, @"[^\w\s]", "");
+            if (indecentSet.Contains(cleanWord))
+            {
+                newText += "**** ";
+            }
+            else
+            {
+                newText += word + " ";
+            }
+        }
+
+        Console.WriteLine($"Filtered text: {newText.Trim()}");
+    }
+}
