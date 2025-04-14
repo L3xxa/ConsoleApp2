49c0aa3c8ec6bb0474b1fa93e17de2830526e56b
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-04-14 20:10:40 +0300

    Add basic calculator functionality to Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter an expression (for example, 10+5-3): ");
+        string expression = Console.ReadLine();
+        
+        int result = 0;
+        int number = 0;
+        char operation = '+';
+
+        for (int i = 0; i <= expression.Length; i++)
+        {
+            if (i < expression.Length && char.IsDigit(expression[i]))
+            {
+                number = number * 10 + (expression[i] - '0');
+            }
+            else if (i == expression.Length || expression[i] == '+' || expression[i] == '-')
+            {
+                if (operation == '+')
+                {
+                    result += number;
+                }
+                else if (operation == '-')
+                {
+                    result -= number;
+                }
+                if (i < expression.Length)
+                    operation = expression[i];
+
+                number = 0;
+            }
+        }
+            
+        Console.WriteLine($"Result: {result}");
+    }
+}
